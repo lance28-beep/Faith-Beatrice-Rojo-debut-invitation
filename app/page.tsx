dc014e0494935221c8f9a2d1dde508fa43001f2e
Rolando Valle <vallelance8@gmail.com>
2026-01-19 23:54:44 +0800

    update entourage

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
         <Messages />
         <Details />
         <Entourage />
-        <PrincipalSponsors />
+        {/* <PrincipalSponsors /> */}
         <GuestList />
         <BookOfGuests />
         {/* <Registry /> */}
