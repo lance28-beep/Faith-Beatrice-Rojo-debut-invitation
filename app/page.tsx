0cd700eada2c8c969014c37d6f4e8bfd10fc4f32
Lance Valle <vallelance8@gmail.com>
2026-02-07 17:44:16 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
               <GuestList />
               <BookOfGuests />
               <FAQ />
-              <Registry />
+              {/* <Registry /> */}
               <SnapShare />
               <Footer />
             </div>
